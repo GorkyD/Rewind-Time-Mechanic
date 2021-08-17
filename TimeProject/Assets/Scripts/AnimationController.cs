9f34e856c358fe08c3163f1d2cc80baf1d1bedb7
Danila Harkavy <d.garkavyy@mail.ru>
2021-08-17 22:46:38 +0300

    Debuging.1

--- TimeProject/Assets/Scripts/AnimationController.cs
+++ TimeProject/Assets/Scripts/AnimationController.cs
@@ -25,7 +25,33 @@ public class AnimationController : MonoBehaviour
     private int _stateBackLeft;
     private int _stateBackRight;
 
-    int StateReqId = Convert.ToInt32("State".GetHashCode());
+    protected const string StateAnimationTrigger = "State";
+    private static readonly int _State = Animator.StringToHash(StateAnimationTrigger);
+
+    protected const string StateBackAnimationTrigger = "StateBack";
+    private static readonly int _StateBack = Animator.StringToHash(StateBackAnimationTrigger);
+
+    protected const string StateRightAnimationTrigger = "StateRight";
+    private static readonly int _StateRight = Animator.StringToHash(StateRightAnimationTrigger);
+
+    protected const string StateLeftAnimationTrigger = "StateLeft";
+    private static readonly int _StateLeft = Animator.StringToHash(StateLeftAnimationTrigger);
+
+    protected const string StateJumpAnimationTrigger = "StateJump";
+    private static readonly int _StateJump = Animator.StringToHash(StateJumpAnimationTrigger);
+
+    protected const string StateForwardLeftAnimationTrigger = "StateForwardLeft";
+    private static readonly int _StateForwardLeft = Animator.StringToHash(StateForwardLeftAnimationTrigger);
+
+    protected const string StateForwardRightAnimationTrigger = "StateForwardRight";
+    private static readonly int _StateForwardRight = Animator.StringToHash(StateForwardRightAnimationTrigger);
+
+    protected const string StateBackLeftAnimationTrigger = "StateBackLeft";
+    private static readonly int _StateBackLeft = Animator.StringToHash(StateBackLeftAnimationTrigger);
+
+    protected const string StateBackRightAnimationTrigger = "StateBackRight";
+    private static readonly int _StateBackRight = Animator.StringToHash(StateBackRightAnimationTrigger);
+
 
     private void Start()
     {
@@ -111,15 +137,15 @@ public class AnimationController : MonoBehaviour
             _stateBackRight = (int)State.None;
         }
 
-        animator.SetInteger(StateReqId, _state);
-        animator.SetInteger("StateBack", _stateBack);
-        animator.SetInteger("StateRight", _stateRight);
-        animator.SetInteger("StateLeft", _stateLeft);
-        animator.SetInteger("StateJump", _stateJump);
-        animator.SetInteger("StateForwardLeft", _stateForLeft);
-        animator.SetInteger("StateForwardRight", _stateForRight);
-        animator.SetInteger("StateBackLeft", _stateBackLeft);
-        animator.SetInteger("StateBackRight", _stateBackRight);
+        animator.SetInteger(_State, _state);
+        animator.SetInteger(_StateBack, _stateBack);
+        animator.SetInteger(_StateRight, _stateRight);
+        animator.SetInteger(_StateLeft, _stateLeft);
+        animator.SetInteger(_StateJump, _stateJump);
+        animator.SetInteger(_StateForwardLeft, _stateForLeft);
+        animator.SetInteger(_StateForwardRight, _stateForRight);
+        animator.SetInteger(_StateBackLeft, _stateBackLeft);
+        animator.SetInteger(_StateBackRight, _stateBackRight);
     }
     
 
